fc0e88bf7802944d43c728f3426f5fa65d872b8c
Adey-W <72650647+Adey-W@users.noreply.github.com>
2020-12-06 22:32:16 +0300

    More of the Checkout

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 function Header() {
+    const [{basket}, ]= useStateValue();
     return (
         <div className='header'>
+            <Link to="/">
             <img  className="header__logo" src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt=""/>
+            </Link>
             <div className="header__search">
         <input className="header__searchInput" type="text"/>
         <SearchIcon className="header__searchIcon" />
@@ -36,12 +41,13 @@ function Header() {
             className='header__optionLine2'>Prime
             </span>            
         </div>
+        <Link to="/checkout">
         <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLine2
-            header_basketCount">0</span>
+            header_basketCount">{basket?.length}</span>
         </div>
-
+        </Link>
         </div>
 
         </div>
